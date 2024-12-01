48dd4c73ff11299911c71126507fcc9c465961b2
Bethlenfalvi, Lorinc (ext) <lorinc.bethlenfalvi.ext@siemens-energy.com>
2024-12-01 19:29:35 +0100

    moved y23 aside

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
 mod boxed_iter;
 mod common;
-mod day1;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day7;
-mod day7b;
-mod day8;
-mod day9;
-mod day10;
-mod day11;
+mod y23;
+mod y24;
 
-fn main() { day11::day11() }
+fn main() { y23::day11::day11() }
